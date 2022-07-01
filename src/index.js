d5a7246228d0e85c96804dcd81f6a7de743f3490
AntonZenko <zenko_anton@ukr.net>
2022-07-01 16:47:14 +0300

    api key,  trending query added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './sass/main.scss';
+import axios from 'axios';
+
+const API_KEY = 'ffda232ba1095b2db867c38e7745d8d7';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+async function fetchGetTrending(pageValue) {
+  const { data } = await axios.get(
+    `/trending/all/day?api_key=${API_KEY}&page=${pageValue}`
+  );
+  const { results, total_pages, page, total_results } = data;
+  return { results, total_pages, page, total_results };
+}
+console.log(fetchGetTrending(1));
